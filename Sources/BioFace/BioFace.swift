09a5462dfe5ce80732236c7854a744b127b5e3b7
Simao Coutinho <simaopedrocoutinho@gmail.com>
2024-03-13 15:21:37 +0000

    update image test

--- Sources/BioFace/BioFace.swift
+++ Sources/BioFace/BioFace.swift
@@ -1,4 +1,5 @@
-
+import Alamofire
+import Foundation
 
 public struct BioFace {
     public private(set) var token = ""
@@ -9,5 +10,25 @@ public struct BioFace {
     
     public func getToken() -> String {
         return token
+    }
+    
+    public func uploadImage(_ file: Data,filename : String) {
+        let headers: HTTPHeaders = [
+            "Content-type": "multipart/form-data",
+            "authorization": "Bearer " + self.token
+        ]
+
+        AF.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(file, withName: "upload_data" , fileName: filename, mimeType: "image/jpg")
+        },
+            to: "https://visteamlab.isr.uc.pt/facing/v2/api/collect", method: .post , headers: headers)
+            .response { response in
+                if let data = response.data{
+                    //handle the response however you like
+                    print(data)
+                }
+
+        }
     }
 }
