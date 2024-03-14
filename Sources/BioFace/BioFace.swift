f76aa3782b2eb1514b0e9160ce930ee2c12df21c
Simao Coutinho <simaopedrocoutinho@gmail.com>
2024-03-14 09:39:56 +0000

    ponto restauro

--- Sources/BioFace/BioFace.swift
+++ Sources/BioFace/BioFace.swift
@@ -3,9 +3,14 @@ import Foundation
 
 public struct BioFace {
     public private(set) var token = ""
+    public private(set) var sessionId = ""
 
     public init(token: String) {
         self.token = token
+    }
+    
+    public mutating func setSessionId(_ mSessionId: String) {
+        sessionId = mSessionId
     }
     
     public func getToken() -> String {
@@ -13,6 +18,9 @@ public struct BioFace {
     }
     
     public func uploadImage(_ file: Data,filename : String) {
+        guard let sessionIdData = sessionId.data(using: .utf8, allowLossyConversion: false) else { return }
+        guard let collectionNameData = "teste anm".data(using: .utf8, allowLossyConversion: false) else { return }
+        
         let headers: HTTPHeaders = [
             "Content-type": "multipart/form-data",
             "authorization": "Bearer " + self.token
@@ -20,7 +28,9 @@ public struct BioFace {
 
         AF.upload(
             multipartFormData: { multipartFormData in
-                multipartFormData.append(file, withName: "upload_data" , fileName: filename, mimeType: "image/jpg")
+                multipartFormData.append(sessionIdData, withName: "session_id")
+                multipartFormData.append(collectionNameData, withName: "collection_name")
+                multipartFormData.append(file, withName: "collection" , fileName: filename, mimeType: "image/jpg")
         },
             to: "https://visteamlab.isr.uc.pt/facing/v2/api/collect", method: .post , headers: headers).responseDecodable(of: Response.self) { response in
                 debugPrint(response)
