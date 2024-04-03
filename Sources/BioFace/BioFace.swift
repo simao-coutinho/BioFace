269d78a35bcbf56e32125108d948021b05fd1799
SimaoCoutinho <simaopedroucoutinho@gmail.com>
2024-04-03 18:02:37 +0100

    added viewController to init

--- Sources/BioFace/BioFace.swift
+++ Sources/BioFace/BioFace.swift
@@ -7,55 +7,53 @@ public class BioFace {
     public static var sessionId: String?
     public static let sharedHandler: BioFace = BioFace()
     
-    private var viewController : BioFaceViewController? = nil
+    private var vc : BioFaceViewController? = nil
     
     private let url = "https://visteamlab.isr.uc.pt/facing/v2/api/"
 
     public init() {}
     
-    public func makeRegistration(_ sessionId: String, completion: @escaping BioFaceResponse) {
+    public func makeRegistration(_ sessionId: String, viewController: UIViewController, completion: @escaping BioFaceResponse) {
         BioFace.sessionId = sessionId
         guard BioFace.apiToken != nil else { return completion(.failed, nil, _error(for: .invalidApiTokenErrorCode)) }
         guard sessionId.data(using: .utf8, allowLossyConversion: false) != nil else {
             return completion(.failed, nil, _error(for: .invalidSessionIdErrorCode))
         }
         
-        viewController = BioFaceViewController.init()
+        vc = BioFaceViewController.init()
         
-        viewController?.setData(serviceType: .makeRegistration, imageResultListener: self, completion: completion)
-        if let rootViewController = UIApplication.shared.keyWindow?.rootViewController, let viewController = viewController {
-                rootViewController.present(viewController, animated: true, completion: nil)
-            }
+        vc?.setData(serviceType: .makeRegistration, imageResultListener: self, completion: completion)
+        viewController.present(viewController, animated: true, completion: nil)
     }
 }
 
 extension BioFace : ImageResultListener {
     func onImageResult(from: ServiceType, with: UIImage, completion: @escaping BioFaceResponse) {
-        viewController?.setProgress(progress: 0, total: 4)
+        vc?.setProgress(progress: 0, total: 4)
         switch from {
         case .makeRegistration:
             let serverConnection = ServerConnection()
             serverConnection.makeImageUpload(with: with) { status, response, error in
-                self.viewController?.setProgress(progress: 1, total: 4)
+                self.vc?.setProgress(progress: 1, total: 4)
                 print("Collect Response: \(response)")
                 switch status {
                 case .succeeded:
                     serverConnection.makeGetConnection(url: "compliance") { status, response, error in
-                        self.viewController?.setProgress(progress: 2, total: 4)
+                        self.vc?.setProgress(progress: 2, total: 4)
                         print("compliance Response: \(response)")
                         switch status {
                         case .succeeded:
                             serverConnection.makeGetConnection(url: "liveness") { status, response, error in
-                                self.viewController?.setProgress(progress: 3, total: 4)
+                                self.vc?.setProgress(progress: 3, total: 4)
                                 print("liveness Response: \(response)")
                                 switch status {
                                 case .succeeded:
                                     serverConnection.makeGetConnection(url: "extract") { status, response, error in
-                                        self.viewController?.setProgress(progress: 1, total: 4)
+                                        self.vc?.setProgress(progress: 1, total: 4)
                                         print("extract Response: \(response)")
                                         switch status {
                                             case .succeeded:
-                                            self.viewController?.dismiss(animated: true)
+                                            self.vc?.dismiss(animated: true)
                                                 completion(.succeeded, nil, nil)
                                             case .canceled:
                                                 completion(.canceled, nil, error)
