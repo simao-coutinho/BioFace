942270093607dc5c992a9fdfed4ae7e72078d17e
SimaoCoutinho <simaopedroucoutinho@gmail.com>
2024-04-21 11:08:31 +0100

    teste

--- Sources/BioFace/Classes/BioFaceServerConnection.swift
+++ Sources/BioFace/Classes/BioFaceServerConnection.swift
@@ -13,13 +13,12 @@ class ServerConnection {
     
     private let url = "https://visteamlab.isr.uc.pt/facing/v2/api/"
     
-    private func getHeaders(sessionId: String) -> HTTPHeaders? {
+    private func getHeaders() -> HTTPHeaders? {
         guard let apiToken = BioFace.apiToken else { return nil}
         
         let headers: HTTPHeaders = [
             "authorization": "Bearer " + apiToken,
-            "Content-Type": "application/x-www-form-urlencoded",
-            "session_id": sessionId
+            "Content-Type": "application/x-www-form-urlencoded"
         ]
         
         return headers
@@ -48,32 +47,7 @@ class ServerConnection {
     }
     
     func makeGetConnection(url: String, sessionId: String, completion: @escaping BioFaceResponse) {
-        /*guard let apiToken = BioFace.apiToken else { return }
-        let parameters = "session_id=\(sessionId)"
-        let postData =  parameters.data(using: .utf8)
-
-        var request = URLRequest(url: URL(string: self.url + url)!,timeoutInterval: Double.infinity)
-        request.addValue(sessionId, forHTTPHeaderField: "session_id")
-        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.addValue("Bearer " + apiToken, forHTTPHeaderField: "Authorization")
-
-        request.httpMethod = "GET"
-        request.httpBody = postData
-
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-
-        let task = session.dataTask(with: request) { data, response, error in
-          guard let data = data else {
-            print(String(describing: error))
-            return
-          }
-          print(String(data: data, encoding: .utf8)!)
-        }
-
-        task.resume()*/
-
-        guard let headers = getHeaders(sessionId: sessionId) else { return }
+        guard let headers = getHeaders() else { return }
         
         AF.request(self.url + url, method: .get, parameters: ["session_id": sessionId], encoding: URLEncoding.queryString, headers: headers).responseString { response in
             print("URL: \(url) -> Response: \(response)")
