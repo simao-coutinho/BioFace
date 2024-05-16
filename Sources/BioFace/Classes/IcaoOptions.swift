57cc6365ba103cb79869414446c405187953d951
SimaoCoutinho <simaopedroucoutinho@gmail.com>
2024-05-16 23:24:57 +0100

    teste

--- Sources/BioFace/Classes/IcaoOptions.swift
+++ Sources/BioFace/Classes/IcaoOptions.swift
@@ -13,31 +13,34 @@ public class IcaoOptions {
     
     private var options : [Int] = []
     
-    let ICAO_EYES_OPENED = 1
-    let ICAO_NEUTRAL_EXPRESSION = 2
-    let ICAO_MOUTH_CLOSED = 3
-    let ICAO_SOULDERS_ALIGNED = 4
-    let ICAO_CORRECT_FACE_POSE = 5
-    let ICAO_LOOKING_FORWARD = 6
-    let ICAO_HAIR_NOT_COVERING_EYES = 7
-    let ICAO_HEAD_WITHOUTH_COVERING = 8
-    let ICAO_VEIL_NOT_OVER_FACE = 9
-    let ICAO_ONLY_ONE_FACE = 10
-    let ICAO_NO_SUNGLASSES = 11
-    let ICAO_FRAMES_NOT_COVERING_EYES = 12
-    let ICAO_NO_GLASSES_REFLECTION = 13
-    let ICAO_FRAMES_NOT_TOO_HEAVY = 14
-    let ICAO_NO_BACKGROUND_SHADOWS = 15
-    let ICAO_NO_FACE_SHADOWS = 16
-    let ICAO_NO_FLASH_REFLECTION = 17
-    let ICAO_NATURAL_SKIN_TONE = 18
-    let ICAO_NO_RED_EYES = 19
-    let ICAO_LIGHTING_CONDITIONS_CORRECT = 20
-    let ICAO_NO_BLURRED_IMAGE = 21
-    let ICAO_HOMOGENEOUS_BACKGROUND = 22
-    let ICAO_NO_PIXELATED_IMAGE = 23
-    let ICAO_NO_WASHED_OUT_IMAGE = 24
-    let ICAO_NO_INK_MARKED_CREASED_IMAGE = 25
+    public enum option: Int {
+        case ICAO_EYES_OPENED = 1
+        case ICAO_NEUTRAL_EXPRESSION = 2
+        case ICAO_MOUTH_CLOSED = 3
+        case ICAO_SOULDERS_ALIGNED = 4
+        case ICAO_CORRECT_FACE_POSE = 5
+        case ICAO_LOOKING_FORWARD = 6
+        case ICAO_HAIR_NOT_COVERING_EYES = 7
+        case ICAO_HEAD_WITHOUTH_COVERING = 8
+        case ICAO_VEIL_NOT_OVER_FACE = 9
+        case ICAO_ONLY_ONE_FACE = 10
+        case ICAO_NO_SUNGLASSES = 11
+        case ICAO_FRAMES_NOT_COVERING_EYES = 12
+        case ICAO_NO_GLASSES_REFLECTION = 13
+        case ICAO_FRAMES_NOT_TOO_HEAVY = 14
+        case ICAO_NO_BACKGROUND_SHADOWS = 15
+        case ICAO_NO_FACE_SHADOWS = 16
+        case ICAO_NO_FLASH_REFLECTION = 17
+        case ICAO_NATURAL_SKIN_TONE = 18
+        case ICAO_NO_RED_EYES = 19
+        case ICAO_LIGHTING_CONDITIONS_CORRECT = 20
+        case ICAO_NO_BLURRED_IMAGE = 21
+        case ICAO_HOMOGENEOUS_BACKGROUND = 22
+        case ICAO_NO_PIXELATED_IMAGE = 23
+        case ICAO_NO_WASHED_OUT_IMAGE = 24
+        case ICAO_NO_INK_MARKED_CREASED_IMAGE = 25
+    }
+    
     
     public func addOption(option: Int) {
         options.append(option)
